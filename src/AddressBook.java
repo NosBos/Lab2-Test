f0082b1f50ae6acabb52e347e7bce09d0ff01755
Harjap Gill <harjap.hg@gmail.com>
2020-09-25 09:16:11 -0400

    Adding and removing from addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,3 +1,5 @@
+import com.sun.jndi.cosnaming.IiopUrl;
+
 import java.util.*;
 public class AddressBook {
 
@@ -17,7 +19,10 @@ public class AddressBook {
 
     public static void main(String[] args) {
 
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton",613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
 
