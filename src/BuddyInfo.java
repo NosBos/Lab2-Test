1803b3f0a2456228dfc101087002557ea6d334bd
Harjap Gill <harjap.hg@gmail.com>
2020-10-02 09:25:37 -0400

    Adding Buddy Info

---
+++ src/BuddyInfo.java
@@ -0,0 +1,30 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private int phoneNumber;
+
+    public BuddyInfo(String name, String address, int phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+
+
+
+
+}
